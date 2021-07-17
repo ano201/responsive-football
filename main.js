1dce13efcfb4e4a238ef61729f020432589b612e
ano201 <ahmu689.com>
2021-07-17 08:13:30 +0600

    updated

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-console.log(innerHeight);
-console.log(innerWidth);
+let allPlayers = document.getElementById("all-players");
+let viewAllButton = document.getElementById("view-all");
+let viewAll = document.getElementById("view-all-button")
+viewAllButton.onclick = function() {
+  allPlayers.setAttribute("style", "width: 90%; height: auto; border: none; overflow: visible;");
+  viewAll.style.display = "none";
+}
